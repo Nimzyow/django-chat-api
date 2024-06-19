6474b46c70d1c34e2aa06d96fa3f425991aa672b
Nima Soufiani <n_soufiani@hotmail.com>
2024-06-19 22:16:24 +0100

    add ability to use regex for url patterns

--- supermessage/chat/urls.py
+++ supermessage/chat/urls.py
@@ -5,12 +5,24 @@ from chat.views import (
     post_message_view,
     start_new_chat_view,
 )
-from django.urls import path
+from django.urls import re_path
 
 urlpatterns = [
-    path("chat", start_new_chat_view, name="chat"),
-    path("chat/1", get_chat_details_view, name="chat"),
-    path("chat/1/message", post_message_view, name="chat"),
-    path("chat/1/messages", get_chat_messages_view, name="chat"),
-    path("chat/1/messages/3", delete_chat_message_view, name="chat"),
+    re_path("^chat/$", start_new_chat_view, name="start_new_chat"),
+    re_path(
+        r"^chat/(?P<chat_id>\d+)/$", get_chat_details_view, name="get_chat_details"
+    ),
+    re_path(
+        r"^chat/(?P<chat_id>\d+)/messages/$",
+        get_chat_messages_view,
+        name="get_chat_messages",
+    ),
+    re_path(
+        r"^chat/(?P<chat_id>\d+)/message/$", post_message_view, name="post_chat_message"
+    ),
+    re_path(
+        r"^chat/(?P<chat_id>\d+)/messages/(?P<message_id>\d+)/$",
+        delete_chat_message_view,
+        name="delete_chat_message",
+    ),
 ]
--- supermessage/chat/views.py
+++ supermessage/chat/views.py
@@ -3,20 +3,26 @@ from django.http import HttpRequest, HttpResponse
 
 # Create your views here.
 def start_new_chat_view(request: HttpRequest):
-    return HttpResponse("start new chat view")
+    return HttpResponse("start new chat view!")
 
 
-def get_chat_details_view(request: HttpRequest):
-    return HttpResponse("Get chat details view")
+def get_chat_details_view(request: HttpRequest, *args, **kwargs):
+    return HttpResponse("Get chat details view! with chat_id of {kwargs['chat_id']}")
 
 
-def post_message_view(request: HttpRequest):
-    return HttpResponse("Post a new message in a chat!")
+def post_message_view(request: HttpRequest, *args, **kwargs):
+    return HttpResponse(
+        "Post a new message in a chat! with chat_id of {kwargs['chat_id']}"
+    )
 
 
-def get_chat_messages_view(request: HttpRequest):
-    return HttpResponse("Get chat messagesss view!")
+def get_chat_messages_view(request: HttpRequest, *args, **kwargs):
+    return HttpResponse(
+        f"Get chat messagesss view! with chat_id of {kwargs['chat_id']}"
+    )
 
 
-def delete_chat_message_view(request: HttpRequest):
-    return HttpResponse("Delete chat message view!")
+def delete_chat_message_view(request: HttpRequest, *args, **kwargs):
+    return HttpResponse(
+        f"Delete chat message view! with chat_id of {kwargs['chat_id']} and a message_id of {kwargs['message_id']}"
+    )
